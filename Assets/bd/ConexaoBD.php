4b46a933545ae405803bc50b586de6f7b776f9f0
willian-hm <willianmoreira0510@gmail.com>
2025-08-14 08:38:25 -0300

    Site com cadastro sem conexão

--- Assets/bd/ConexaoBD.php
+++ Assets/bd/ConexaoBD.php
@@ -4,6 +4,8 @@ class ConexaoBD
     public static function conectar():PDO
     {        
 
-        return new PDO("pgsql:host=localhost;dbname=projeto-programacao", "postgres", "1234");
+        $conexao = new PDO("pgsql:host=localhost;dbname=projeto-programacao", "postgres", "1234");
+
+         return $conexao;
     }
 }
